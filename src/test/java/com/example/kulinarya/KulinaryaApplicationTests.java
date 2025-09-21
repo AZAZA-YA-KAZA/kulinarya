cabbb736ee207f1adca7133198348e02ef5e14a9
Aswertti <var.yerkhova@gmail.com>
2025-09-21 18:09:59 +0500

    доп файлы

---
+++ src/test/java/com/example/kulinarya/KulinaryaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kulinarya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KulinaryaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
